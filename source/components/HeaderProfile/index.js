d19651a7fd3a35f635b76af8d015ad3ca65108b9
HeshamElgammal <90568429+HeshamElgammal@users.noreply.github.com>
2022-08-26 22:45:30 +0200

    ..

--- source/components/HeaderProfile/index.js
+++ source/components/HeaderProfile/index.js
@@ -6,15 +6,19 @@ import LinearGradient from 'react-native-linear-gradient';
 const HeaderProfile = () => {
   return (
     <>
-    <LinearGradient colors={[MainColors.blue,MainColors.blue,MainColors.gray]} 
-    // start={{ x: 0, y: 0 }} end={{ x: 1, y: 2 }}
-    style={styles.container}>
-      <Text style={{color: MainColors.light, marginTop: sizes.height * 0.1}}>
-        HeaderProfile
-      </Text>
-   
-    </LinearGradient>
-    <View style={{width:"100%",height:1,marginTop:-sizes.height*.15}}></View>
+      <LinearGradient
+        colors={[MainColors.blue, MainColors.blue, MainColors.gray]}
+        style={styles.container}>
+        <Text style={{color: MainColors.light, marginTop: sizes.height * 0.1}}>
+          HeaderProfile
+        </Text>
+      </LinearGradient>
+      <View
+        style={{
+          width: '100%',
+          height: 1,
+          marginTop: -sizes.height * 0.15,
+        }}></View>
     </>
   );
 };
